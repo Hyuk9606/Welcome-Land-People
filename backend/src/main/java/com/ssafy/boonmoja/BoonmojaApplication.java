01a378e075c827232d712139510ef06ad9d4c2da
LimHyuk <skdltm117@gmail.com>
2022-03-06 22:46:58 +0900

    Feat: backend Springboot initialization

---
+++ backend/src/main/java/com/ssafy/boonmoja/BoonmojaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.boonmoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoonmojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoonmojaApplication.class, args);
+	}
+
+}
