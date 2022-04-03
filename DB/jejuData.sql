f2c5826280a21669e71f5925ed83aa77ed5c3922
LimHyuk <skdltm117@gmail.com>
2022-04-03 19:44:29 +0900

    Chore: sql오류 수정

--- DB/jejuData.sql
+++ DB/jejuData.sql
@@ -37,7 +37,7 @@
 --   `title` varchar(50) DEFAULT NULL,
 --   PRIMARY KEY (`contents_id`)
 -- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `contents`
