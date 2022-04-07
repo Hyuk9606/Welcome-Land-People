1b49b81a0bed49a9062f9d549fb1bce7ebebc7f0
doyeon <kiixdox@gmail.com>
2022-04-07 21:32:33 +0900

    Feat : create rankingAPI

---
+++ backend/src/main/java/com/ssafy/boonmoja/api/dto/RankDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.boonmoja.api.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class RankDto {
+    private String rank_num;
+    private String rank_voca;
+}
